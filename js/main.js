c945114dbe3ea2df511ecba586332372347c27ec
Mustafa Emre Ozmen <mustafa.eozmen@gmail.com>
2021-01-28 17:24:12 +0300

    Added timeout for winning.

--- js/main.js
+++ js/main.js
@@ -1,67 +1,72 @@
-const gameTable = document.querySelector('.game-table');
-const gameButton = document.querySelector('#game-button');
-const tdArray = document.getElementsByTagName('td');
-const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const gameTable = document.querySelector('.game-table')
+const gameButton = document.querySelector('#game-button')
+const winnerSection = document.querySelector('#winner')
+const tdArray = document.getElementsByTagName('td')
+const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const winConditions = [
-    [gameBoard[0], gameBoard[1], gameBoard[2]],
-    [gameBoard[3], gameBoard[4], gameBoard[5]],
-    [gameBoard[6], gameBoard[7], gameBoard[8]],
-    [gameBoard[0], gameBoard[3], gameBoard[6]],
-    [gameBoard[1], gameBoard[4], gameBoard[7]],
-    [gameBoard[2], gameBoard[5], gameBoard[8]],
-    [gameBoard[0], gameBoard[4], gameBoard[8]],
-    [gameBoard[2], gameBoard[4], gameBoard[6]]
-];
+  [gameBoard[0], gameBoard[1], gameBoard[2]],
+  [gameBoard[3], gameBoard[4], gameBoard[5]],
+  [gameBoard[6], gameBoard[7], gameBoard[8]],
+  [gameBoard[0], gameBoard[3], gameBoard[6]],
+  [gameBoard[1], gameBoard[4], gameBoard[7]],
+  [gameBoard[2], gameBoard[5], gameBoard[8]],
+  [gameBoard[0], gameBoard[4], gameBoard[8]],
+  [gameBoard[2], gameBoard[4], gameBoard[6]]
+]
 
-let turn = 'X';
-let gameStatus = false;
+let turn = 'X'
+let gameStatus = false
 let clicksOf = {
-    'X': [],
-    'O': []
+  'X': [],
+  'O': []
 }
 
 const gameStatusChanger = () => {
-    if (gameStatus) {
-        gameButton.className = 'not-started';
-        gameButton.innerHTML = 'start game';
-        resetGame();
-    }
-    else {
-        gameButton.className = 'started';
-        gameButton.innerHTML = 'end game';
-    }
-    gameStatus = !gameStatus;
+  if (gameStatus) {
+    gameButton.className = 'not-started';
+    gameButton.innerHTML = 'start game';
+    resetGame();
+  }
+  else {
+    gameButton.className = 'started';
+    gameButton.innerHTML = 'end game';
+  }
+  gameStatus = !gameStatus;
 }
 
 const checkWinner = (clicks) => {
-    return winConditions.filter((condition) => {
-        return condition.sort().toString() == clicks.sort().toString()
-    }).length;
+  return winConditions.filter(condition => 
+    condition.sort().toString() == clicks.sort().toString()
+  ).length;
 }
 
 const boardCellClick = function (cell) {
-    if (gameStatus && !isNaN(cell.innerText)) {
-        clicksOf[turn].push(cell.innerText);
-        cell.innerText = turn;
-        debugger;
-        if(checkWinner(clicksOf[turn]))
-            alert(turn + " wins!")
-        turn = turn == 'X' ? 'O' : 'X';
+  if (gameStatus && !isNaN(cell.innerText)) {
+    clicksOf[turn].push(cell.innerText);
+    cell.innerText = turn;
+    debugger;
+    if (checkWinner(clicksOf[turn])){
+      winnerSection.innerHTML = `${turn} wins!`;
+      setTimeout(
+        () => gameStatusChanger(), 5000
+      );
     }
+    turn = turn == 'X' ? 'O' : 'X';
+  }
 }
 
 const resetGame = () => {
-    window.location.reload();
+  window.location.reload();
 }
 
 window.addEventListener('load', (e) => {
-    let length = gameBoard.length;
-    for (let i = 0; i < length; i++) {
-        tdArray[i].innerText = gameBoard[i];
-        tdArray[i].onclick = (e) => boardCellClick(tdArray[i]);
-    }
+  let length = gameBoard.length;
+  for (let i = 0; i < length; i++) {
+    tdArray[i].innerText = gameBoard[i];
+    tdArray[i].onclick = (e) => boardCellClick(tdArray[i]);
+  }
 });
 
 gameButton.addEventListener('click', (e) => {
-    gameStatusChanger();
+  gameStatusChanger();
 });
